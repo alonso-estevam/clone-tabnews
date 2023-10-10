76d440565f6c31e20f17b5ae55ad571a33a2d746
Alonso Estevam <alonso_estevam@outlook.com>
2023-10-10 11:22:01 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return <div>
+                <h1>Nenhum Valdinei vai estragar seu dia</h1>
+    
+                <h2>Mas em caso de emergência, assista os vídeos abaixo:</h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/XvJ2OzcBVQg?si=4YQuKbUypY87tQWG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/K1M_mZX7ceU?si=xH4Cj84FW6VPBJ3c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/0_N99uB__wE?si=rQbBN3pdt8HeLgz4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+             </div>
+}
+
+export default Home;
