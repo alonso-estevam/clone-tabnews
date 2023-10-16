d5127bd2ff592956dd3e132d6cba4951cd31d86c
Alonso Estevam <alonso_estevam@outlook.com>
2023-10-16 16:38:55 +0000

    substitui video indisponivel

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ function Home() {
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/EZ6BQDNHbdc?si=E0XXDSvNdM5tMiu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/0_N99uB__wE?si=rQbBN3pdt8HeLgz4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/xPShlKutlnU?si=8T-EfdEXbosdnKNq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ekgSie3qxNM?si=FNr7k5QBUDRRlaKU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/-IRzh9GtNyg?si=5UCNpmP0sUBiJzqY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
              </div>
 }
 
