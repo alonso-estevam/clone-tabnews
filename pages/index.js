f145d7a78d9de337784f775cb7281f5b56aba729
Alonso Estevam <alonso_estevam@outlook.com>
2023-10-16 16:04:22 +0000

    adiciona mais videos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 function Home() {
     return <div>
                 <h1>Nenhum Valdinei vai estragar seu dia</h1>
-    
+                    
                 <h2>Mas em caso de emergência, assista os vídeos abaixo:</h2>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/XvJ2OzcBVQg?si=4YQuKbUypY87tQWG" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/K1M_mZX7ceU?si=xH4Cj84FW6VPBJ3c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/EZ6BQDNHbdc?si=E0XXDSvNdM5tMiu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/0_N99uB__wE?si=rQbBN3pdt8HeLgz4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/xPShlKutlnU?si=8T-EfdEXbosdnKNq" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ekgSie3qxNM?si=FNr7k5QBUDRRlaKU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
              </div>
 }
 
